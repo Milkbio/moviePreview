0773e6b4af306c9b46cd52cc942bc29760e8e072
Milkbio <1032635701@qq.com>
2020-01-04 16:07:13 +0800

    update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
 const Koa = require('koa')
-const App = new Koa()
+const logger = require('koa-logger')
+const app = new Koa()
 
-App.use(async (ctx, next) => {
-    ctx.body = 'Hi luke'
-}).listen(3333)
+
+const mid1 = async (ctx, next) => {
+  ctx.type = 'text/html; charset=utf-8'
+  await next()
+}
+
+const mid2 = async (ctx, next) => {
+  ctx.body = 'Hi'
+  await next()
+}
+
+const mid3 = async (ctx, next) => {
+  ctx.body += ', luke'
+  // await next()
+}
+
+app.use(logger())
+app.use(mid1)
+app.use(mid2)
+app.use(mid3)
+
+app.listen(3333)
