4dc82dd66220cd02e56b14011fad4f228559afc0
Milkbio <1032635701@qq.com>
2020-01-04 17:52:15 +0800

    加入pug模板，并通过koa-views中间件来解析渲染pug

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const Koa = require('koa')
 const logger = require('koa-logger')
-const ejs = require('ejs')
-const {htmlTpl, ejsTpl} = require('./tpl/')
+const views = require('koa-views')
+const {resolve} = require('path')
 
 const app = new Koa()
 
 app.use(logger())
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
 app.use(async (ctx, next) => {
-  ctx.body = ejs.render(ejsTpl, {
+  await ctx.render('index', {
     you: 'Luke',
     me: 'Scott'
   })
